e219bfadc6e86df293b3be59db2faaf8166db678
Denisio Rodrigues <denisio@ymail.com>
2023-01-29 10:24:09 -0300

    Layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'  
+import { DefaultLeyout } from './layouts/DefaultLayout';
 
 import { History } from './pages/History'
 import { Home } from './pages/Home'
@@ -6,8 +7,10 @@ import { Home } from './pages/Home'
 export function Router() {
  return (
   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/history" element={<History/>}/>
+    <Route path="/" element={<DefaultLeyout />}>
+      <Route path="/" element={<Home />}/>
+      <Route path="/history" element={<History />}/>
+    </Route>
   </Routes>
  ); 
 }
