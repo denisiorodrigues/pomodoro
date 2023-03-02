94354739ac800bc30a30e9d3255a4df320971a51
Denisio Rordrigues <denisio@ymail.com>
2023-03-02 00:35:44 -0300

    Formatação de data no histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from "react"
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from "../../contexts/CyclesContext"
 import { HistoryContainer, HistoryList, Status } from "./styles"
 
@@ -26,7 +28,10 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>  {/*Há cerca de 2 meses */}
+                  <td>{ formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR
+                  }) }</td>  {/*Há cerca de 2 meses */}
                   <td>
                     {cycle.finishedDate && <Status statusColor="green">Concluído</Status>}
                     {cycle.interruptedDate && <Status statusColor="red">Interrompido</Status>}
